eb61a8ac04c7f12fed183d3d02ab7ab20c60d53a
silentbutmad <mohitmahajan489@gmail.com>
2025-04-13 21:57:12 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,31 @@
+
+
+var number=10;
+console.log(number);
+console.log(typeof(number));
+
+
+/* DataType 
+
+    primitive -
+            Number
+            String
+            Boolean
+            undefined  - Represent the absence of a value or an uninitialized variable
+            BigInt
+            Number
+            null
+
+*/
+
+/*
+    convert String to a number
+        
+        1) just add the '+' sign before string
+        2) Number() constructor
+
+*/
+var number="10"
+console.log(typeof(number))
+console.log(typeof(+number))
+console.log(typeof(Number(number)))
