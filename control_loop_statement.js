dd0943e4ead050cc6c069966b0303f24b35f0f30
silentbutmad <mohitmahajan489@gmail.com>
2025-04-14 14:45:49 +0530

    Main

---
+++ control_loop_statement.js
@@ -0,0 +1,105 @@
+/*
+        if statement
+
+    if()
+    {
+
+    }
+
+        if-else 
+
+    if()
+    {
+    
+    }else
+    {
+    
+    }
+
+        if-else-if
+    
+    if()
+    {
+    
+    }else if()
+    {
+    
+    }else
+    {
+
+    }
+
+        Switch
+
+    switch()
+    {
+        case value1:
+
+            break;
+
+        case value2:
+
+            break;
+
+        case value3:
+
+            break;
+
+        case value4:
+
+            break;
+        
+        default:
+
+    }
+
+
+
+*/
+
+/*
+
+        while() loop
+    
+    while()
+    {
+    
+    }
+
+
+        do-while() loop
+    
+    do
+    {
+
+    }while();
+
+
+        for() loop
+    
+    for(initializer; condition; iteration)
+    {
+
+
+    }
+
+*/
+
+// print pattern
+// *
+// **
+// ***
+// ****
+// *****
+
+for(var row=1;row<5;row++)
+{
+    var line="";
+    for(var col=1;col<=row;col++)
+    {
+        line+="*";
+        
+    }
+    console.log(line);
+    
+}
